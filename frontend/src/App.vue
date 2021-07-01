b0e291e60d2f9d4519f6a1c43e01bd00fb6d6f50
nerdthejohn <newtonjohn043@gmail.com>
2021-07-01 18:31:19 +0100

    landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-    <div class="nav-item">
-          <router-link to="/" class="nav-link">
-            Home
+  <div class='overflow-hidden'>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between fixed-top">
+      <a class="navbar-brand" href="#">
+        <img :src="require(`@/assets/logo.png`)" class="d-inline-block brand-image" alt="logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active" data-toggle="collapse" data-target=".navbar-collapse.show">
+            <router-link to="/" class="nav-link sr-only">
+              Home
+            </router-link>
+          </li>
+          <li class="nav-item dropdown" data-toggle="collapse" data-target=".navbar-collapse.show">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Fundraise For
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Health</a>
+              <a class="dropdown-item" href="#">Emergency</a>
+              <a class="dropdown-item" href="#">Education</a>
+              <a class="dropdown-item" href="#">Charity</a>
+            </div>
+          </li>
+          <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+            <a class="nav-link" href="#">How It Works</a>
+          </li>
+        </ul>
+        <span class="form-inline my-2 my-lg-0 ml-auto">
+          <button class="btn btn-outline-success my-2 my-sm-0 mr-4 signup" data-toggle="collapse" data-target=".navbar-collapse.show">
+            Sign Up
+          </button>
+          <router-link to="/login" class="btn btn-success my-2 my-sm-0 ml-4 signin" data-toggle="collapse" data-target=".navbar-collapse.show">
+            Sign In
           </router-link>
-          <router-link to="/login" class="nav-link">
-            Login
-          </router-link>
-          <div class="container">
-            <router-view></router-view>
-          </div>
+        </span>
+      </div>
+    </nav>
+    <div class="">
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -18,6 +50,14 @@
 <script>
 
 
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
+library.add(faUserSecret);
 
 export default {
   name: 'App',
@@ -28,6 +68,7 @@ export default {
 </script>
 
 <style>
+
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -35,5 +76,79 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+
+  .navbar{
+    padding: 0 20px;
+    margin: 0;
+    width: 100%;
+  }
+
+  .brand-image{
+      height: auto;
+      width: 250px;
+  }
+
+
+  .navbar-nav{
+    margin-left: auto;
+    margin-right: 20px;
+  }
+
+  .signup{
+    margin-right: 15px;
+  }
+
+  .navbar-light .navbar-toggler{
+    border: none;
+    outline: none;
+  }
+  
+  .navbar-light .navbar-toggler:focus{
+    border: none;
+    outline: none;
+    box-shadow: none;
+  }
+
+  .navbar-light .navbar-toggler-icon{
+    background-image: none;
+    height: 2px;
+    width: 24px;
+    position: relative;
+    background-color: green;
+  }
+
+  .navbar-light .navbar-toggler-icon::before{
+    content:'';
+    position: absolute;
+    height: 2px;
+    width: 100%;
+    left: 0;
+    top: -6px;
+    background-color: green;
+  }
+
+  .navbar-light .navbar-toggler-icon::after{
+    content:'';
+    position: absolute;
+    height: 2px;
+    width: 100%;
+    left: 0;
+    top: 6px;
+    background-color: green;
+}
+
+  .navbar-toggler {
+      background-color: green;
+      margin-right: 10px;
+      padding: 7px;
+      
+  }
+
+  .navbar-toggler:focus{
+      box-shadow: none;
+      outline: none;
+      border: none;
   }
 </style>
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,19 +1,86 @@
 <template>
-  <div>
-    <div class="mt-4">
-      <h1>Landing Page (Vue/Django)</h1>
+  <div class='overflow-hidden'>
+    <div class="home container">
+      <div class="row">
+        <div class="col-lg-6 col-md-9 col-sm-12">
+          <div class="">
+            <h1 class='top-text'>Fundraising for the people and causes you care about.</h1>
+            <h4 class='mid-text mt-4 mb-4'>Get Started Today.</h4>
+            <a class='d-flex justify-content-start btn btn-lg btn-success my-btn text-light' href='#'>Start a Fundraiser</a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-3 d-none d-lg-block d-md-block">
+          <img :src="require(`@/assets/5240.jpg`)" class='img img-fluid myimg'>
+        </div>
+      </div>
     </div> 
+    <div class="carousel-section container">
+      <carousel :items-to-show="2" :wrap-around='true'>
+        <Slide v-for="image in images" :key="image">
+          <img :src=image.link class='img img-fluid slider-img'>
+          <p class='text-muted mt-4 img-text'>{{image.text}}</p>
+        </Slide>
+
+        <template #addons>
+          <Navigation />
+        </template>
+      </carousel>
+    </div>
+    <div class="featured-section">
+      <div class="container">
+        <h3 class='text-success'>Popular Fundraisers</h3> 
+        <div class="fundraisers mt-4 container">
+          <div class="row">
+            <div v-for="campaign in popular" :key="campaign" class="col-lg-3 col-sm-6 campaign justify-content-center">
+                <img :src=campaign.media class='img img-fluid campaign-image'>
+                <div class='categories'>
+                  <span v-for="category in campaign.categories" :key="category" class='category'>{{category}}</span>
+                </div>
+                <h4 class='text-muted align-left campaign-title'>{{campaign.title}}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Navigation } from 'vue3-carousel';
+
 export default {
   name: 'Home',
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  },
+  data(){
+    return {
+      images:[
+        {'link': require(`@/assets/1.png`),'text':'Over 4000 Nigerian kids are not in school, start a fundraising campaign.'},
+        {'link': require(`@/assets/2.png`),'text':'Fundraise to provide food and shelter for the homeless and needy.'},
+        {'link': require(`@/assets/3.jpg`),'text':'You have plans to study abroad? Open a fundraiser and share to your friends.'},
+        {'link': require(`@/assets/4.jpg`),'text':'Start a campaign to raise money for yourself and others during this lockdown.'},
+        {'link': require(`@/assets/5.jpg`),'text':'Help a friend raise money for surgery or medication'}
+      ],
+      popular: [
+        {'media': require(`@/assets/1.png`), 'title': "Ifunaya's Heart Transplant", 'categories': ['Health','Emergency'],'percentage': 30, 'amount_raised': 200000, 'deadline': '3 days left'},
+        {'media': require(`@/assets/4.jpg`), 'title': "Let's get Ayo to Harvard Business School", 'categories': ['Education'],'percentage': 60, 'amount_raised': 1360000, 'deadline': '1 month left'},
+        {'media': require(`@/assets/2.png`), 'title': "Operation Send 100 Kogi Kids to School", 'categories': ['Education','Charity'],'percentage': 70, 'amount_raised': 105750, 'deadline': '2 weeks left'},
+        {'media': require(`@/assets/5.jpg`), 'title': "Help Jay get a laptop to code", 'categories': ['Tech','Education'],'percentage': 15, 'amount_raised': 12050, 'deadline': '2 months left'}
+      ]
+    }
+  }
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+
 h3 {
   margin: 40px 0 0;
 }
@@ -28,4 +95,110 @@ li {
 a {
   color: #42b983;
 }
+
+.row {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+
+.mid-text,
+.top-text{
+  text-align: left;
+}
+
+.top-text{
+  font-display: bold;
+  font-weight: 600;
+  font-size: 40px;
+  color: #3A3838;
+}
+
+.mid-text{
+  font-weight: 400;
+  font-size: 30px;
+  color: #3A3838;
+}
+
+.my-btn{
+  width: 220px;
+  padding: 7px 20px;
+  margin-top: 45px;
+}
+
+.home{
+  padding: 100px 20px;
+}
+
+.myimg{
+  max-height: 100%;
+
+}
+
+.featured-section{
+  padding: 30px 0px 50px 0px;
+  background: rgb(213, 245, 213);
+}
+
+.carousel-section{
+  padding: 100px 0px;
+}
+
+.slider-img{
+  min-height: 150px;
+  max-height: 300px;
+  border-radius: 15px;
+}
+
+.fundraisers{
+  padding: 30px 0px;
+}
+
+.campaign{
+  height: 400px;
+}
+
+.campaign-title{
+  text-align: left;
+  margin-top: 10px;
+}
+
+.campaign-image{
+  height: 200px;
+}
+
+.categories{
+  margin-top: 10px;
+  text-align: left;
+}
+
+.category{
+  margin: 0px 4px 0px 0px;
+  padding: 6px 9px;
+  border-radius: 2px;
+  font-size: 10px;
+  background: green;
+  color: #ffffff;
+  cursor: pointer;
+  transition: 0.2s ease-in;
+}
+
+.category:hover{
+  transition: 0.2s ease-out;
+}
+
+
+@media screen and (max-width: 450px) {
+  .carousel__prev,
+  .carousel__next{
+    margin: 100px;
+  }
+
+  .img-text{
+    font-size: 10px;
+  }
+}
+
 </style>
